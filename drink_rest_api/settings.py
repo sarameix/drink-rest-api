8ee940ed2e590ec4da8a6685072d3a55092f20c7
smeixner <sarameix22@gmail.com>
2022-09-15 15:54:39 -0400

    hiding secret key

--- drink_rest_api/settings.py
+++ drink_rest_api/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yac9*sh-s8(qn-b2ovy0l7c7xp$c_8twq03u(3wwm!geen82zx'
+SECRET_KEY = os.getenv('SECRET_KEY', 'Optional default value')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
